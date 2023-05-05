0ad97ba0dc5b8a7cdd91bf3fa90b8b13849c8db3
mhawkinsbasis <113123911+mhawkinsbasis@users.noreply.github.com>
2023-05-05 15:48:40 +0200

    feat: added label demo

---
+++ src/main/java/control_demos/labeldemos/LabelSample.java
@@ -0,0 +1,30 @@
+package control_demos.labeldemos;
+
+import org.dwcj.App;
+import org.dwcj.annotation.InlineStyleSheet;
+import org.dwcj.component.window.Frame;
+import org.dwcj.component.label.Label;
+import org.dwcj.exceptions.DwcjException;
+import org.dwcj.component.TextAlignable.Alignment;
+
+@InlineStyleSheet("context://css/labelstyles/text_styles.css")
+public class LabelSample extends App {
+  @Override
+  public void run() throws DwcjException {
+    Frame app = new Frame();
+    app.addClassName("Frame");
+
+    Label myLabel = new Label("Hover over me!");
+    myLabel.setTextAlignment(Alignment.MIDDLE);
+    
+    myLabel.addMouseEnterListener( e -> {
+      myLabel.setText("Mouse Entered at X coordinate: " + e.getScreenX() + " and Y coordinate: " + e.getScreenY());
+    });
+
+    myLabel.addMouseExitListener( e -> {
+      myLabel.setText("Hover over me!");
+    });
+
+    app.add(myLabel);
+  }
+}
