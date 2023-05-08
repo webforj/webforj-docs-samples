abbae9e20828ae873c9a802ab1fb85aa9f932472
mhawkinsbasis <113123911+mhawkinsbasis@users.noreply.github.com>
2023-05-08 14:01:41 +0200

    feat: Created collapsed code snipped, changed alignment dialog

--- src/main/java/control_demos/dialogdemos/DialogAlignments.java
+++ src/main/java/control_demos/dialogdemos/DialogAlignments.java
@@ -2,8 +2,7 @@ package control_demos.dialogdemos;
 
 import org.dwcj.App;
 import org.dwcj.annotation.InlineStyleSheet;
-import org.dwcj.component.button.Button;
-import org.dwcj.component.button.event.ButtonClickEvent;
+import org.dwcj.component.choicebox.ChoiceBox;
 import org.dwcj.component.dialog.Dialog;
 import org.dwcj.component.label.Label;
 import org.dwcj.component.window.Frame;
@@ -11,37 +10,30 @@ import org.dwcj.exceptions.DwcjException;
 
 @InlineStyleSheet("context://css/dialogstyles/dialog_styles.css")
 public class DialogAlignments extends App {
-  Button alignTop;
-  Button alignMid;
-  Button alignBtm;
   Dialog dialog;
+  ChoiceBox options;
 
   @Override
   public void run() throws DwcjException {
     Frame p = new Frame();
     dialog = new Dialog();
     p.add(dialog);
-
-    alignTop = new Button("Align to Top").onClick(this::topAlign);
-    alignMid = new Button("Align to Center").onClick(this::midAlign);
-    alignBtm = new Button("Align to Bottom").onClick(this::btmAlign);
+    options = new ChoiceBox()
+      .addItem(Dialog.Alignment.TOP, "Top")
+      .addItem(Dialog.Alignment.CENTER, "Center")
+      .addItem(Dialog.Alignment.BOTTOM, "Bottom")
+      .setAttribute("label", "Select Alignment")
+      .selectIndex(1)
+      .onSelect( e -> {
+        dialog.setAlignment((Dialog.Alignment) e.getKey());
+      });
 
     dialog.getHeader().add(new Label("Alignments"));
-    dialog.getContent().add(alignTop, alignMid, alignBtm)
-      .setStyle("display","flex")
-      .setStyle("justify-content", "space-around");
+    dialog.getContent().add(options)
+    .setStyle("display", "flex")
+    .setStyle("justify-content", "center");
 
     dialog.show();
     dialog.setCloseable(false);
-  }
-
-  private void topAlign(ButtonClickEvent ev){
-    dialog.setAlignment(Dialog.Alignment.TOP);
-  }
-  private void midAlign(ButtonClickEvent ev){
-    dialog.setAlignment(Dialog.Alignment.CENTER);
-  }
-  private void btmAlign(ButtonClickEvent ev){
-    dialog.setAlignment(Dialog.Alignment.BOTTOM);
   }
 }
