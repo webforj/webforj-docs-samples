5b171fdc772c4e112fc726e172e89fad1b835d1a
mhawkinsbasis <113123911+mhawkinsbasis@users.noreply.github.com>
2023-06-13 11:03:13 +0200

    feat: Updated the mobile applayout demos

--- src/main/java/layout_demos/applayout/AppLayoutMobileDrawer.java
+++ src/main/java/layout_demos/applayout/AppLayoutMobileDrawer.java
@@ -13,7 +13,7 @@ import org.dwcj.component.window.Panel;
 import org.dwcj.exceptions.DwcjException;
 
 @InlineStyleSheet("context://css/applayoutstyles/applayout_mobile.css")
-public class AppLayoutMobile extends App{
+public class AppLayoutMobileDrawer extends App{
   
   Label contentLabel; 
   
@@ -24,14 +24,43 @@ public class AppLayoutMobile extends App{
       panel.add(demo);
 
       //Header 
-      demo.getHeader().add(new Label("<html><div class=\"bbj-logo\"><img src=\"https://i.ibb.co/1n4n1Nh/logo.png\" alt=\"logo\" /></div></html>"))
-          .add(new Label("DWCJ Application")
+      demo.getHeader()
+          .add(new Label("<html><bbj-icon-button name='menu-2' data-drawer-toggle><bbj-icon-button></html>"))
+          .add(new Label("<html><div class=\"bbj-logo\"><img src=\"https://i.ibb.co/1n4n1Nh/logo.png\" alt=\"logo\" /></div></html>"))
+          .add(new Label("<html><b>DWCJ Application</b></html>")
         )
         .addClassName("bbj-toolbar");
 
       demo.setHeaderReveal(true);
 
-      demo.setDrawerPlacement(DrawerPlacement.HIDDEN);
+
+
+      demo.setDrawerPlacement(DrawerPlacement.LEFT);
+      Panel drawer = demo.getDrawer();
+      drawer.addClassName("app-layout-drawer");
+
+      //Drawer's logo container and logo
+      drawer.add(new Panel().addClassName("drawer__logo").add(
+          new Label("<html><img src='" + "https://i.ibb.co/1n4n1Nh/logo.png" + "'</img></html>")
+      ));
+
+      //Drawer's Menu
+      TabbedPane drawerMenu = new TabbedPane();
+      drawer.add(drawerMenu);
+      
+      //Setting drawer menu's attributes
+      drawerMenu.setAttribute("nobody","true");
+      drawerMenu.setAttribute("borderless","true");
+      drawerMenu.setAttribute("placement","left");
+
+      //Adding tabs to drawer menu
+      drawerMenu.add("<bbj-icon name='dashboard'></bbj-icon>      Dashboard")
+          .add("<bbj-icon name='shopping-cart'></bbj-icon>  Orders"   )
+          .add("<bbj-icon name='users'></bbj-icon>          Customers")
+          .add("<bbj-icon name='box'></bbj-icon>            Products" )
+          .add("<bbj-icon name='files'></bbj-icon>          Documents")
+          .add("<bbj-icon name='checklist'></bbj-icon>      Tasks"    )
+          .add("<bbj-icon name='chart-dots-2'></bbj-icon>   Analytics");
 
       //Content
       this.contentLabel = new Label();
