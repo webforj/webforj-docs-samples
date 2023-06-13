0c2e48e5e445722bdf9864bc56a629e477f49c41
mhawkinsbasis <113123911+mhawkinsbasis@users.noreply.github.com>
2023-06-13 10:14:19 +0200

    feat: Added DWCJ code for the AppLayout demos

---
+++ src/main/java/layout_demos/applayout/AppLayoutMobile.java
@@ -0,0 +1,82 @@
+package layout_demos.applayout;
+
+import org.dwcj.App;
+import org.dwcj.annotation.InlineStyleSheet;
+import org.dwcj.annotation.StyleSheet;
+import org.dwcj.component.applayout.AppLayout;
+import org.dwcj.component.applayout.AppLayout.DrawerPlacement;
+import org.dwcj.component.label.Label;
+import org.dwcj.component.tabbedpane.TabbedPane;
+import org.dwcj.component.tabbedpane.event.TabSelectEvent;
+import org.dwcj.component.window.Frame;
+import org.dwcj.component.window.Panel;
+import org.dwcj.exceptions.DwcjException;
+
+@InlineStyleSheet("context://css/applayoutstyles/applayout_mobile.css")
+public class AppLayoutMobile extends App{
+  
+  Label contentLabel; 
+  
+  @Override
+    public void run() throws DwcjException{
+      Frame panel = new Frame();
+      AppLayout demo = new AppLayout();
+      panel.add(demo);
+
+      //Header 
+      demo.getHeader().add(new Label("<html><div class=\"bbj-logo\"><img src=\"https://i.ibb.co/1n4n1Nh/logo.png\" alt=\"logo\" /></div></html>"))
+          .add(new Label("DWCJ Application")
+        )
+        .addClassName("bbj-toolbar");
+
+      demo.setHeaderReveal(true);
+
+      demo.setDrawerPlacement(DrawerPlacement.HIDDEN);
+
+      //Content
+      this.contentLabel = new Label();
+      demo.getContent().add(
+          new Label("<html><h1>Application Title</h1></html>"),
+          this.contentLabel
+          );
+      for(int i = 0; i < 5; i++){
+        demo.getContent().add(
+          new Label("<html>" + 
+              "<div class=\"card\">" +
+                "<h2>What is Lorem Ipsum " + i + "?</h2>" +
+                "<p>" +
+                  "Lorem Ipsum is simply dummy text of the printing and typesetting" +
+                  "industry. Lorem Ipsum has been the industry's standard dummy text" +
+                  "ever since the 1500s when an unknown printer took a galley of type" +
+                  "and scrambled it to make a type specimen book. It has survived not" +
+                  "only five centuries, but also the leap into electronic" +
+                  "typesetting, remaining essentially unchanged. It was popularized" +
+                  "in the 1960s with the release of Letraset sheets containing Lorem" +
+                  "Ipsum passages, and more recently with desktop publishing software" +
+                  "like Aldus PageMaker including versions of Lorem Ipsum." +
+                "</p>" +
+              "</div>" +
+            "</html>"
+          )
+        );
+      }
+      
+      TabbedPane drawerMenu = new TabbedPane();
+      demo.getFooter().add(drawerMenu);
+      demo.setFooterReveal(true);
+      
+      //Setting drawer menu's attributes
+      drawerMenu.setAttribute("nobody","true");
+      drawerMenu.setAttribute("borderless","true");
+      drawerMenu.setAttribute("placement","bottom");
+      drawerMenu.setAttribute("alignment","stretch");
+      
+      //Adding tabs to drawer menu
+      drawerMenu.add("<bbj-icon name='dashboard'></bbj-icon>")
+          .add("<bbj-icon name='shopping-cart'></bbj-icon>")
+          .add("<bbj-icon name='users'></bbj-icon>" )
+          .add("<bbj-icon name='box'></bbj-icon>")
+          .add("<bbj-icon name='files'></bbj-icon>");
+  }
+    
+}
--- src/main/java/layout_demos/applayout/AppLayoutStickyToolbar.java
+++ src/main/java/layout_demos/applayout/AppLayoutStickyToolbar.java
@@ -10,8 +10,8 @@ import org.dwcj.component.window.Frame;
 import org.dwcj.component.window.Panel;
 import org.dwcj.exceptions.DwcjException;
 
-@InlineStyleSheet("context://css/applayoutstyles/applayout_styles.css")
-public class AppLayoutMultipleHeaders extends App{
+@InlineStyleSheet("context://css/applayoutstyles/applayout_sticky_styles.css")
+public class AppLayoutStickyToolbar extends App{
   
   Label contentLabel; 
   
@@ -27,6 +27,7 @@ public class AppLayoutMultipleHeaders extends App{
           new Label("DWCJ Application")
               .addClassName("layout__header--title")
       );
+      demo.setHeaderReveal(true);
 
       //Drawer
       Panel drawer = demo.getDrawer();
@@ -62,7 +63,28 @@ public class AppLayoutMultipleHeaders extends App{
       demo.getContent().add(
           new Label("<html><h1>Application Title</h1></html>"),
           this.contentLabel
-      );
+          );
+      for(int i = 0; i < 10; i++){
+        demo.getContent().add(
+          new Label("<html>" + 
+              "<div class=\"card\">" +
+                "<h2>What is Lorem Ipsum " + i + "?</h2>" +
+                "<p>" +
+                  "Lorem Ipsum is simply dummy text of the printing and typesetting" +
+                  "industry. Lorem Ipsum has been the industry's standard dummy text" +
+                  "ever since the 1500s when an unknown printer took a galley of type" +
+                  "and scrambled it to make a type specimen book. It has survived not" +
+                  "only five centuries, but also the leap into electronic" +
+                  "typesetting, remaining essentially unchanged. It was popularized" +
+                  "in the 1960s with the release of Letraset sheets containing Lorem" +
+                  "Ipsum passages, and more recently with desktop publishing software" +
+                  "like Aldus PageMaker including versions of Lorem Ipsum." +
+                "</p>" +
+              "</div>" +
+            "</html>"
+          )
+        );
+      }
 
       // Adding the additional toolbar with menu items
       Panel secondToolbar = new Panel()
