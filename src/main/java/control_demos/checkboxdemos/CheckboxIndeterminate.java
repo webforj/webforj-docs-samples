dc1f89c17dd0406b86e7b09203410cb4d8c615d9
mhawkinsbasis <113123911+mhawkinsbasis@users.noreply.github.com>
2023-06-22 15:41:52 +0200

    Updated CheckBox demo

--- src/main/java/control_demos/checkboxdemos/CheckboxIndeterminate.java
+++ src/main/java/control_demos/checkboxdemos/CheckboxIndeterminate.java
@@ -3,20 +3,20 @@ package control_demos.checkboxdemos;
 import org.dwcj.App;
 import org.dwcj.annotation.InlineStyleSheet;
 import org.dwcj.component.window.Frame;
-import org.dwcj.component.TextPosition.Position;
 import org.dwcj.component.checkbox.CheckBox;
 import org.dwcj.exceptions.DwcjException;
 
 @InlineStyleSheet("context://css/checkboxstyles/text_styles.css")
-public class CheckboxHorizontalText extends App {
+public class CheckboxIndeterminate extends App {
   @Override
   public void run() throws DwcjException {
     Frame panel = new Frame();
     panel.addClassName("Frame");
 
-    CheckBox cb1 = new CheckBox().setText("Right aligned (default)");
-    CheckBox cb2 = new CheckBox().setText("Left aligned").setTextPosition(Position.LEFT);
+    CheckBox cb1 = new CheckBox().setText("Checked").setChecked(true);
+    CheckBox cb2 = new CheckBox().setText("Unchecked").setChecked(false);
+    CheckBox cb3 = new CheckBox().setText("Indeterminate").setIndeterminate(true);
 
-    panel.add(cb1, cb2);
+    panel.add(cb1, cb2, cb3);
   }
 }
