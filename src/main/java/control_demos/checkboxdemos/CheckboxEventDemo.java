9b7df9b7ca6e49b49da16a2743a2ade5139cacbe
mhawkinsbasis <113123911+mhawkinsbasis@users.noreply.github.com>
2023-06-28 13:51:46 +0200

    Updated checkbox to include an event demo

---
+++ src/main/java/control_demos/checkboxdemos/CheckboxEventDemo.java
@@ -0,0 +1,28 @@
+package control_demos.checkboxdemos;
+
+import org.dwcj.App;
+import org.dwcj.annotation.InlineStyleSheet;
+import org.dwcj.component.checkbox.CheckBox;
+import org.dwcj.component.window.Frame;
+import org.dwcj.exceptions.DwcjException;
+
+@InlineStyleSheet("context://css/checkboxstyles/text_styles.css")
+public class CheckboxEventDemo extends App {
+  
+  CheckBox checkbox;
+
+  @Override
+  public void run() throws DwcjException {
+
+    Frame panel = new Frame()
+      .addClassName("Frame");
+    checkbox = new CheckBox();
+    panel.add(checkbox);
+
+    checkbox.onCheck( e -> checkbox.setText("Checkbox is Checked!") );
+    
+    checkbox.onUncheck( e -> checkbox.setText("Checkbox is Unchecked!") );
+
+  }
+  
+}
