e0fa8e74d4903877baa7ce2d2c2a5677c9bf2be6
mhawkinsbasis <113123911+mhawkinsbasis@users.noreply.github.com>
2023-08-28 16:49:45 +0200

    feat: Added passwordfield demo

---
+++ src/main/java/componentdemos/fielddemos/PasswordFieldDemo.java
@@ -0,0 +1,27 @@
+package componentdemos.fielddemos;
+
+import org.dwcj.App;
+import org.dwcj.annotation.InlineStyleSheet;
+import org.dwcj.component.field.PasswordField;
+import org.dwcj.component.window.Frame;
+import org.dwcj.exceptions.DwcjAppInitializeException;
+
+@InlineStyleSheet("context://css/fieldstyles/date_field_styles.css")
+public class PasswordFieldDemo extends App {
+
+  @Override
+  public void run() throws DwcjAppInitializeException {
+
+    Frame window = new Frame();
+    window.addClassName("main__window");
+
+    PasswordField passwordField = new PasswordField();
+    passwordField.addClassName("date__input");
+    passwordField.setLabel("Enter your Password:");
+    passwordField.setPlaceholder("Password");
+
+    window.add(passwordField);
+
+  }
+}
+
