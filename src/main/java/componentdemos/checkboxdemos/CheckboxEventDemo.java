b30678c80801e89ad35a15e3a6287b2e97c6744e
mhawkinsbasis <113123911+mhawkinsbasis@users.noreply.github.com>
2023-10-10 10:30:10 +0200

    Added text files for contracted code

--- src/main/java/componentdemos/checkboxdemos/CheckboxEventDemo.java
+++ src/main/java/componentdemos/checkboxdemos/CheckboxEventDemo.java
@@ -13,7 +13,6 @@ public class CheckboxEventDemo extends App {
 
   @Override
   public void run() throws DwcjException {
-
     Frame window = new Frame()
         .addClassName("Frame");
     checkbox = new CheckBox()
