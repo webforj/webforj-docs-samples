3ab65d5ca83e89f82de636054cc1c5a6f2b9a034
mhawkinsbasis <113123911+mhawkinsbasis@users.noreply.github.com>
2023-10-31 11:39:51 +0100

    Added combobox demo and changed styling

---
+++ src/main/java/componentdemos/textcomboboxdemos/ComboBoxCustomValue.java
@@ -0,0 +1,29 @@
+package componentdemos.textcomboboxdemos;
+
+import org.dwcj.App;
+import org.dwcj.annotation.InlineStyleSheet;
+import org.dwcj.component.button.Button;
+import org.dwcj.component.list.ComboBox;
+import org.dwcj.component.window.Frame;
+import org.dwcj.exceptions.DwcjException;
+
+// @InlineStyleSheet("context://css/textcomboboxstyles/text_combo_styles.css")
+public class ComboBoxCustomValue extends App {
+  @Override
+  public void run() throws DwcjException {
+    Frame window = new Frame();
+    window.addClassName("Frame");
+
+    ComboBox customValue = new ComboBox();
+    Button toggle = new Button("Toggle Custom Value");
+
+    window.add(customValue, toggle);
+    customValue.setAllowCustomValue(false);
+
+    toggle.onClick( e -> {
+      customValue.setAllowCustomValue(!customValue.isAllowCustomValue());
+      consoleLog(customValue.isAllowCustomValue()+"");
+    });
+
+  }
+}
---
+++ src/main/java/componentdemos/textcomboboxdemos/ComboBoxDemo.java
@@ -0,0 +1,31 @@
+package componentdemos.textcomboboxdemos;
+
+import org.dwcj.App;
+import org.dwcj.annotation.InlineStyleSheet;
+import org.dwcj.component.list.ComboBox;
+import org.dwcj.component.window.Frame;
+import org.dwcj.exceptions.DwcjAppInitializeException;
+
+@InlineStyleSheet("context://css/textcomboboxstyles/demo_styles.css")
+public class ComboBoxDemo extends App {
+
+    @Override
+    public void run() throws DwcjAppInitializeException {
+
+        Frame window = new Frame();
+        window.addClassName("frame");
+
+        ComboBox categories = new ComboBox();
+        window.add(categories);
+
+        categories.add("Electronics","Electronics");
+        categories.add("Health and Beauty","Health and Beauty");
+        categories.add("Fashion","Fashion");
+        categories.add("Kitchen","Kitchen");
+        categories.add("Furniture","Furniture");
+        categories.add("Pet Supplies","Pet Supplies");
+        categories.add("Toys and Games","Toys and Games");
+
+        categories.setLabel("Type or Select Category");
+    }
+}
