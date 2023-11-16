978c189fbbdd7241fc7640b9e3bac7f6e0f553b0
mhawkinsbasis <113123911+mhawkinsbasis@users.noreply.github.com>
2023-11-16 13:34:56 +0100

    Added placement type for combobox

---
+++ src/main/java/componentdemos/textcomboboxdemos/ComboBoxDropdownType.java
@@ -0,0 +1,21 @@
+package componentdemos.textcomboboxdemos;
+
+import org.dwcj.App;
+import org.dwcj.annotation.InlineStyleSheet;
+import org.dwcj.component.window.Frame;
+import org.dwcj.component.list.ComboBox;
+import org.dwcj.exceptions.DwcjAppInitializeException;
+
+@InlineStyleSheet("context://css/comboboxstyles/dropdown_styles.css")
+public class ComboBoxDropdownType extends App {
+
+    @Override
+    public void run() throws DwcjAppInitializeException {
+        Frame window = new Frame();
+        window.addClassName("frame");
+
+        ComboBox demoBox = new ComboBox();
+        demoBox.setDropdownType("demo-dropdown-type");
+        window.add(demoBox);
+    }
+}
