807c70af2d39a9b4436e958dd147265464c5acec
MatthewHawkins <matthew.hawkins1115@gmail.com>
2023-11-28 15:09:38 +0100

    Cleaning up code for list demos

--- src/main/java/componentdemos/comboboxdemos/ChoiceBoxDropdownType.java
+++ src/main/java/componentdemos/comboboxdemos/ChoiceBoxDropdownType.java
@@ -9,42 +9,15 @@ import org.dwcj.component.list.ChoiceBox;
 import org.dwcj.component.list.ListItem;
 import org.dwcj.exceptions.DwcjException;
 
-@InlineStyleSheet(/* css */"""
+@InlineStyleSheet("context://css/comboboxstyles/dropdown_styles.css")
 
-  :root{
-    /*Overriding list item color to make hover more noticeable*/
-    --bbj-list-item-color: #393D48;
-  }
-
-  .frame {
-    display: flex;
-    margin: 20px 0 0 20px;
-    width: 200px;
-  }
-
-  [data-dropdown-for="demo-dropdown-type"]::part(list){
-    transition: transform 0.3s ease; /* Smooth transition for the transform effect */
-    overflow: hidden;
-  }
-
-  [data-dropdown-for="demo-dropdown-type"]::part(item) {
-    transition: transform 0.3s ease, background-color 0.3s ease;
-  }
-
-  [data-dropdown-for="demo-dropdown-type"]::part(item):hover{
-    transition: transform 0.3s ease; /* Smooth transition for the transform effect */
-    transform: scale(1.2) translateX(10px); /* Scale up the list item on hover */
-    cursor: pointer; /* Change cursor to indicate interactivity */
-  }
-""")
-public class ChoiceBoxDropdownType extends App {
+public class ChoiceBoxDropdownType extends App {  
+  ChoiceBox demoBox = new ChoiceBox("Department");
 
   @Override
   public void run() throws DwcjException {
     Frame window = new Frame();
     window.addClassName("frame");
-
-    ChoiceBox demoBox = new ChoiceBox();
     window.add(demoBox);
 
     ArrayList<ListItem> categories = new ArrayList<>();
--- src/main/java/componentdemos/textcomboboxdemos/ComboBoxDropdownType.java
+++ src/main/java/componentdemos/textcomboboxdemos/ComboBoxDropdownType.java
@@ -1,21 +1,36 @@
 package componentdemos.textcomboboxdemos;
 
+import java.util.ArrayList;
+
 import org.dwcj.App;
 import org.dwcj.annotation.InlineStyleSheet;
 import org.dwcj.component.window.Frame;
 import org.dwcj.component.list.ComboBox;
+import org.dwcj.component.list.ListItem;
 import org.dwcj.exceptions.DwcjException;
 
-@InlineStyleSheet("context://css/textcomboboxstyles/dropdown_styles.css")
+@InlineStyleSheet("context://css/comboboxstyles/dropdown_styles.css")
 public class ComboBoxDropdownType extends App {
 
-    @Override
-    public void run() throws DwcjException {
-        Frame window = new Frame();
-        window.addClassName("frame");
+  ComboBox demoBox = new ComboBox("Department");
 
-        ComboBox demoBox = new ComboBox();
-        demoBox.setDropdownType("demo-dropdown-type");
-        window.add(demoBox);
-    }
+  @Override
+  public void run() throws DwcjException {
+    Frame window = new Frame();
+    window.addClassName("frame");
+    window.add(demoBox);
+
+    ArrayList<ListItem> categories = new ArrayList<>();
+    categories.add(new ListItem("Electronics", "Electronics"));
+    categories.add(new ListItem("Health and Beauty", "Health and Beauty"));
+    categories.add(new ListItem("Fashion", "Fashion"));
+    categories.add(new ListItem("Kitchen", "Kitchen"));
+    categories.add(new ListItem("Furniture", "Furniture"));
+    categories.add(new ListItem("Pet Supplies", "Pet Supplies"));
+    categories.add(new ListItem("Toys and Games", "Toys and Games"));
+
+    demoBox.setDropdownType("demo-dropdown-type");
+    demoBox.insert(categories)
+        .selectIndex(0);
+  }
 }
