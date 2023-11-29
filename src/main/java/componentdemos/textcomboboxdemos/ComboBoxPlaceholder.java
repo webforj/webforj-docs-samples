05cc44cc2c05647280b15dd831b486de096b1f1e
MatthewHawkins <matthew.hawkins1115@gmail.com>
2023-11-29 11:36:13 +0100

    updating demo code

--- src/main/java/componentdemos/textcomboboxdemos/ComboBoxPlaceholder.java
+++ src/main/java/componentdemos/textcomboboxdemos/ComboBoxPlaceholder.java
@@ -4,30 +4,27 @@ import java.util.ArrayList;
 
 import org.dwcj.App;
 import org.dwcj.annotation.InlineStyleSheet;
+import org.dwcj.component.window.Frame;
 import org.dwcj.component.list.ComboBox;
 import org.dwcj.component.list.ListItem;
-import org.dwcj.component.optioninput.RadioButton;
-import org.dwcj.component.window.Frame;
 import org.dwcj.exceptions.DwcjException;
 
-@InlineStyleSheet(/*css*/"""
-  .frame{
-    display: flex;
-    margin: 20px 0 0 20px;
-    gap: 20px;
-  }
-""")
-public class ComboBoxCustomValue extends App {
-  
-  ComboBox customValue = new ComboBox();
-  RadioButton toggle = RadioButton.Switch("Toggle Custom Value");
+@InlineStyleSheet(/* css */"""
+    .frame{
+      display: flex;
+      margin: 20px 0 0 20px;
+      gap: 20px;
+    }
+    """)
+public class ComboBoxPlaceholder extends App {
+
+  ComboBox comboBox = new ComboBox();
 
   @Override
   public void run() throws DwcjException {
     Frame window = new Frame();
     window.addClassName("frame");
-    window.add(customValue, toggle);
-    
+
     ArrayList<ListItem> categories = new ArrayList<>();
     categories.add(new ListItem("Electronics", "Electronics"));
     categories.add(new ListItem("Health and Beauty", "Health and Beauty"));
@@ -36,15 +33,9 @@ public class ComboBoxCustomValue extends App {
     categories.add(new ListItem("Furniture", "Furniture"));
     categories.add(new ListItem("Pet Supplies", "Pet Supplies"));
     categories.add(new ListItem("Toys and Games", "Toys and Games"));
-    
-    customValue.setDropdownType("demo-dropdown-type");
-    customValue.insert(categories);
-    customValue.setAllowCustomValue(false);
 
-    toggle.onToggle( e -> {
-      customValue.setAllowCustomValue(!customValue.isAllowCustomValue());
-      consoleLog(customValue.isAllowCustomValue()+"");
-    });
-
+    comboBox.setPlaceholder("Example Placeholder");
+    comboBox.insert(categories);
+    window.add(comboBox);
   }
 }
