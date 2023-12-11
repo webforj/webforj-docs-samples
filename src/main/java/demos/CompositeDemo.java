73ea6010e44323ebcd8bcd2f176478fabff4a292
MatthewHawkins <matthew.hawkins1115@gmail.com>
2023-12-11 16:43:14 +0100

    Updated the composite demo based on Hyyan's feedback

--- src/main/java/demos/CompositeDemo.java
+++ src/main/java/demos/CompositeDemo.java
@@ -3,70 +3,68 @@ package demos;
 import org.dwcj.App;
 import org.dwcj.annotation.InlineStyleSheet;
 import org.dwcj.component.Composite;
+import org.dwcj.component.Expanse;
 import org.dwcj.component.event.KeypressEvent;
 import org.dwcj.component.field.TextField;
 import org.dwcj.component.html.elements.Div;
+import org.dwcj.component.html.elements.H1;
 import org.dwcj.component.layout.flexlayout.FlexAlignment;
 import org.dwcj.component.layout.flexlayout.FlexLayout;
-import org.dwcj.component.optioninput.CheckBox;
+import org.dwcj.component.optioninput.RadioButton;
 import org.dwcj.component.window.Frame;
 import org.dwcj.exceptions.DwcjException;
 
-@InlineStyleSheet(/* css */"""
-      .frame{
-        display: flex;
-        flex-direction: column;
-        gap: 20px;
-        margin: 20px;
-      }
-    """)
+@InlineStyleSheet("context://css/compositestyles/styles.css")
 public class CompositeDemo extends App {
 
-  TextField text = new TextField("Todo Item");
+  TextField text = new TextField();
   FlexLayout todoDisplay;
+  H1 title = new H1("Todos");
 
   @Override
   public void run() throws DwcjException {
     Frame window = new Frame();
-    window.addClassName("frame");
+    window.addClassName("frame")
+        .add(title, todoDisplay);
 
-    todoDisplay = FlexLayout.create()
+    todoDisplay = FlexLayout.create(text)
         .vertical()
         .build()
-        .setSpacing("5px");
-
-    window.add(text, todoDisplay);
+        .setSpacing("5px")
+        .addClassName("todo--display");
 
     text.setPlaceholder("Add Todo item. Press Enter to save.")
-        .setStyle("max-width", "50%")
+        .setExpanse(Expanse.XLARGE)
         .onKeypress(e -> {
           if (e.getKeyCode().equals(KeypressEvent.Key.ENTER) && !text.getText().isEmpty()) {
             todoDisplay.add(new TodoItem(text.getText()));
             text.setText("");
           }
         });
+
+    todoDisplay.add(new TodoItem("Groceries"),
+        new TodoItem("Water Plants"),
+        new TodoItem("Exercise"));
   }
 
   public static class TodoItem extends Composite<FlexLayout> {
 
-    CheckBox checkbox = new CheckBox();
+    RadioButton radioButton = RadioButton.Switch();
     Div text = new Div();
 
     TodoItem(String todoText) {
       this.text.setText(todoText);
-    }
+      getBoundComponent().setSpacing("3px")
+          .setAlignment(FlexAlignment.CENTER)
+          .addClassName("item__todo--display")
+          .add(radioButton, text);
 
-    @Override
-    protected void onDidCreate(FlexLayout container) {
-      container.add(checkbox, text);
-      container.setSpacing("3px")
-          .setAlignment(FlexAlignment.CENTER);
-
-      checkbox.onCheck(e -> {
-        text.setStyle("text-decoration", "line-through");
-      });
-      checkbox.onUncheck(e -> {
-        text.setStyle("text-decoration", "unset");
+      radioButton.onToggle(e -> {
+        if (e.isToggled()) {
+          text.setStyle("text-decoration", "line-through");
+        } else {
+          text.setStyle("text-decoration", "unset");
+        }
       });
     }
   }
