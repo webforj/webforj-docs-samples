9f36c3cdddccbb3f0a2d684d7f2a4b0ffba4c0bb
MatthewHawkins <matthew.hawkins1115@gmail.com>
2023-12-14 15:20:51 +0100

    Updating adding

--- src/main/java/demos/webcomponents/DetailsSlots.java
+++ src/main/java/demos/webcomponents/DetailsSlots.java
@@ -6,21 +6,22 @@ import org.dwcj.component.window.Frame;
 import org.dwcj.exceptions.DwcjException;
 
 public class DetailsSlots extends App {
+  
+  Element details = new Element("details");
+  Element secondDetails = new Element("details");
+
   @Override
   public void run() throws DwcjException {
     Frame window = new Frame();
     window.setStyle("margin", "20px");
+    window.add(details, secondDetails);
 
-    Element details = new Element("details");
-    window.add(details);
-
+    /* Method 1: Setting text using setText() and add()*/
     Element summary = new Element("summary", "First Details Title");
     details.setText("Expanded text for the first summary."); // Sets the content inside the details element
     details.add(summary); // Adds a summary element to the text already present inside the details element
 
-    Element secondDetails = new Element("details");
-    window.add(secondDetails);
-
+    /* Method 2: Injecting pure HTML into the element*/
     secondDetails.setHtml("""
       <summary>Second Details Title</summary>
       Expanded text for the second summary.
