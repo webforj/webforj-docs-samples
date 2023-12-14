28c9a038576e0869c4224bb41ab59bcfe11bc85d
MatthewHawkins <matthew.hawkins1115@gmail.com>
2023-12-14 15:05:09 +0100

    Updating detail demo

--- src/main/java/demos/webcomponents/DetailsSlots.java
+++ src/main/java/demos/webcomponents/DetailsSlots.java
@@ -14,7 +14,8 @@ public class DetailsSlots extends App {
     Element details = new Element("details");
     window.add(details);
 
-    Element summary = new Element("summary", "This is a Summary");
-    details.add(summary);
+    Element summary = new Element("summary", "Details Title");
+    details.setHtml("Something small enough to escape casual notice."); // Sets the text to be displayed when expanded
+    details.add(summary); // Sets the title of the details element
   }
 }
