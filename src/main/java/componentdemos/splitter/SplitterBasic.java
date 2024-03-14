bc2f8ebf34dc6d16039efc4c10edd05fd29b14db
Hyyan Abo Fakher <hyyanaf@gmail.com>
2024-03-14 16:49:43 +0100

    feat: add splitter demos

---
+++ src/main/java/componentdemos/splitter/SplitterBasic.java
@@ -0,0 +1,20 @@
+package componentdemos.splitter;
+
+import com.webforj.App;
+import com.webforj.component.layout.splitter.Splitter;
+import com.webforj.component.window.Frame;
+import com.webforj.exceptions.WebforjException;
+
+public class SplitterBasic extends App {
+
+  @Override
+  public void run() throws WebforjException {
+    Frame mainFrame = new Frame();
+
+    SplitterBox master = new SplitterBox("Master", SplitterBox.Theme.INFO);
+    SplitterBox detail = new SplitterBox("Detail", SplitterBox.Theme.SUCCESS);
+    Splitter splitter = new Splitter(master, detail);
+
+    mainFrame.add(splitter);
+  }
+}
