e42213de8dca08afc1ca1b69a6380bcc0f6e47ed
Hyyan Abo Fakher <hyyanaf@gmail.com>
2024-03-15 10:07:56 +0100

    refactor: update SplitterAutoSave demo to include cleanState

--- src/main/java/componentdemos/splitter/SplitterAutoSave.java
+++ src/main/java/componentdemos/splitter/SplitterAutoSave.java
@@ -1,6 +1,11 @@
 package componentdemos.splitter;
 
 import com.webforj.App;
+import com.webforj.Page;
+import com.webforj.component.button.Button;
+import com.webforj.component.element.Element;
+import com.webforj.component.layout.flexlayout.FlexAlignment;
+import com.webforj.component.layout.flexlayout.FlexLayout;
 import com.webforj.component.layout.splitter.Splitter;
 import com.webforj.component.window.Frame;
 import com.webforj.exceptions.WebforjException;
@@ -9,7 +14,6 @@ public class SplitterAutoSave extends App {
 
   @Override
   public void run() throws WebforjException {
-    Frame mainFrame = new Frame();
 
     SplitterBox topLeft = new SplitterBox("Top Left", SplitterBox.Theme.PRIMARY);
     SplitterBox topRight = new SplitterBox("Top Right", SplitterBox.Theme.INFO);
@@ -27,6 +31,25 @@ public class SplitterAutoSave extends App {
     masterSplitter.setOrientation(Splitter.Orientation.VERTICAL);
     masterSplitter.setAutosave(true);
 
-    mainFrame.add(masterSplitter);
+    Button clearState = new Button("Clear State");
+    clearState.addClickListener(e -> {
+      topSplitter.cleanState();
+      bottomSplitter.cleanState();
+      masterSplitter.cleanState();
+      Page.getCurrent().reload();
+    });
+
+    Element reload = new Element("dwc-icon-button");
+    reload.setAttribute("name", "reload");
+    reload.setAttribute("theme", "primary");
+    reload.addEventListener("click", e -> Page.getCurrent().reload());
+
+    FlexLayout toolbar = FlexLayout.create(reload, clearState).horizontal().build();
+    FlexLayout layout = FlexLayout.create(toolbar, masterSplitter).vertical().build();
+    layout.setStyle("padding", "var(--dwc-space-m)");
+    layout.setItemAlignment(FlexAlignment.END, toolbar);
+
+    Frame mainFrame = new Frame();
+    mainFrame.add(layout);
   }
 }
