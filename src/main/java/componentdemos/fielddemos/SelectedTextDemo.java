7930a83eeb1bf737936c8d358aefa6b1b87ebd20
cliebhardt <liebhardt.christian@gmail.com>
2024-04-04 15:18:08 +0200

    Added SelectedTextDemo.java to showcase using selected text.

---
+++ src/main/java/componentdemos/fielddemos/SelectedTextDemo.java
@@ -0,0 +1,56 @@
+package componentdemos.fielddemos;
+
+import com.webforj.App;
+import com.webforj.annotation.InlineStyleSheet;
+import com.webforj.component.Theme;
+import com.webforj.component.button.Button;
+import com.webforj.component.button.ButtonTheme;
+import com.webforj.component.field.TextField;
+import com.webforj.component.html.elements.Paragraph;
+import com.webforj.component.text.Label;
+import com.webforj.component.window.Frame;
+import com.webforj.exceptions.WebforjException;
+
+@InlineStyleSheet("""
+  .window {
+    display: flex;
+    align-items: flex-end;
+    margin: 20px;
+    gap: 50px;
+    width: 100%;
+    flex-wrap: wrap;
+  }
+  """)
+public class SelectedTextDemo extends App {
+  private final TextField input;
+  private final Button confirm;
+  private final Paragraph message;
+
+  public SelectedTextDemo() {
+    input = new TextField(TextField.Type.TEXT)
+      .setValue("Try selecting 'DANGER' to change the button.");
+    confirm = new Button("Get Selection");
+    message = new Paragraph();
+    message.setVisible(false);
+  }
+
+  @Override
+  public void run() throws WebforjException {
+    Frame window = new Frame();
+    window.addClassName("window");
+    window.add(input, confirm, message);
+
+    confirm.addClickListener(e -> {
+      String selection = input.getSelectedText();
+      try {
+        message.setVisible(false);
+        ButtonTheme theme = ButtonTheme.valueOf(selection);
+        confirm.setTheme(theme);
+      } catch (IllegalArgumentException __) {
+        message.setText("Selected: " + selection);
+        message.setVisible(true);
+      }
+    });
+  }
+
+}
