5a5abbc612d4a393b73f00ed49028f6509132033
RaptorBarry <liebhardt.christian@gmail.com>
2024-04-04 13:09:53 +0200

    Added Demo for TimeField

---
+++ src/main/java/componentdemos/fielddemos/TimeFieldDemo.java
@@ -0,0 +1,26 @@
+package componentdemos.fielddemos;
+
+import com.webforj.App;
+import com.webforj.annotation.InlineStyleSheet;
+import com.webforj.component.field.TimeField;
+import com.webforj.component.window.Frame;
+import com.webforj.exceptions.WebforjException;
+
+import java.time.LocalTime;
+
+@InlineStyleSheet("context://css/fieldstyles/time_field_styles.css")
+public class TimeFieldDemo extends App {
+
+
+  @Override
+  public void run() throws WebforjException {
+    Frame window = new Frame();
+    window.addClassName("main__window");
+
+    TimeField reminder = new TimeField("Set Reminder:", LocalTime.now());
+    reminder.addClassName("time__input");
+
+    window.add(reminder);
+  }
+
+}
---
+++ src/main/java/componentdemos/fielddemos/TimeFieldMinMax.java
@@ -0,0 +1,53 @@
+package componentdemos.fielddemos;
+
+import com.webforj.App;
+import com.webforj.annotation.InlineStyleSheet;
+import com.webforj.component.button.Button;
+import com.webforj.component.button.ButtonTheme;
+import com.webforj.component.field.TimeField;
+import com.webforj.component.window.Frame;
+import com.webforj.exceptions.WebforjException;
+
+import java.time.LocalTime;
+
+@InlineStyleSheet("""
+  .window {
+    display: flex;
+    align-items: flex-end;
+    margin: 20px;
+    gap: 50px;
+    width: 100%;
+    flex-wrap: wrap;
+  }
+  """)
+public class TimeFieldMinMax extends App {
+  private final TimeField meeting;
+  private final Button confirm;
+
+  public TimeFieldMinMax() {
+    meeting = new TimeField(LocalTime.of(12, 30));
+    confirm = new Button("Confirm");
+    confirm.setTheme(ButtonTheme.PRIMARY).setEnabled(false);
+  }
+
+  @Override
+  public void run() throws WebforjException {
+    Frame window = new Frame();
+    window.addClassName("window");
+    window.add(meeting, confirm);
+
+    LocalTime min = LocalTime.of(12, 30);
+    LocalTime max = LocalTime.of(17, 15);
+    String label = "Choose a meeting time between %s and %s:".formatted(min, max);
+    meeting.setLabel(label)
+      .onModify(e -> {
+        try {
+          meeting.setText(e.getText());
+          confirm.setEnabled(true);
+        } catch (IllegalArgumentException __) {
+          confirm.setEnabled(false);
+        }
+      });
+  }
+
+}
