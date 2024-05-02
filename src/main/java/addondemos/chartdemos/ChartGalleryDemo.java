68822586a7d56ae4ed81f18ef6474ee27faaa17d
lauren <laurenalamo@outlook.com>
2024-05-01 23:50:41 -0400

    feat: adding check to avoid null error and labels for each chart

--- src/main/java/addondemos/chartdemos/ChartGalleryDemo.java
+++ src/main/java/addondemos/chartdemos/ChartGalleryDemo.java
@@ -5,12 +5,11 @@ import com.google.gson.reflect.TypeToken;
 import com.webforj.App;
 import com.webforj.annotation.InlineStyleSheet;
 import com.webforj.component.googlecharts.GoogleChart;
+import com.webforj.component.html.elements.Div;
+import com.webforj.component.html.elements.Paragraph;
 import com.webforj.component.window.Frame;
 import com.webforj.exceptions.WebforjException;
 import com.webforj.utilities.Assets;
-import java.time.LocalDate;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -34,13 +33,38 @@ public class ChartGalleryDemo extends App {
         GoogleChart chart = new GoogleChart(GoogleChart.Type.valueOf(chartKey.toUpperCase()));
         Map<String, Object> data = entry.getValue();
         Map<String, Object> options = chartOptions.get(chartKey);
+
         if (data != null && options != null) {
+            Div chartDiv = new Div();
+            chartDiv.addClassName("chart-div");
+
+            Paragraph chartName = new Paragraph();
+            String formattedTitle = formatTitle(chartKey);
+            chartName.setText(formattedTitle);
+            chartName.addClassName("chartname");
+            chartDiv.add(chartName);
+
             chart.setOptions(options);
             chart.setData((List<Object>) data.get("data"));
-            window.add(chart);
+            chartDiv.add(chart);
+
+            window.add(chartDiv);
+        }
+      }
+   }
+
+  private String formatTitle(String title) {
+    if (title == null || title.isEmpty()) return title;
+    String[] words = title.replace("_", " ").split("\\s+");
+    StringBuilder formattedTitle = new StringBuilder();
+
+    for (String word : words) {
+        if (word.length() > 0) {
+            formattedTitle.append(Character.toUpperCase(word.charAt(0)))
+                          .append(word.substring(1).toLowerCase())
+                          .append(" ");
         }
     }
-      
-   }
- 
+    return formattedTitle.toString().trim() + " Chart";
+  }
 }
