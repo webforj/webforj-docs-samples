2d2f12429b4efb7b62c757c07cf7fc5578ca35cb
Ben Brennan <bbrennan@basis.cloud>
2024-07-02 15:48:07 -0600

    Progress updating ComboboxThemeDemo Reduced the amount off css being used.

--- src/main/java/componentdemos/choiceboxdemos/ComboboxThemeDemo.java
+++ src/main/java/componentdemos/choiceboxdemos/ComboboxThemeDemo.java
@@ -4,25 +4,40 @@ import java.util.ArrayList;
 import java.util.List;
 import com.webforj.App;
 import com.webforj.component.window.Frame;
-import com.webforj.component.button.Button;
+import com.webforj.component.layout.flexlayout.FlexLayout;
+import com.webforj.component.layout.flexlayout.FlexLayoutBuilder;
 import com.webforj.component.button.ButtonTheme;
 import com.webforj.component.list.ChoiceBox;
 import com.webforj.component.list.ListItem;
 import com.webforj.exceptions.WebforjException;
 import com.webforj.annotation.InlineStyleSheet;
 
-@InlineStyleSheet("context://css/comboboxstyles/theme_styles.css")
-public class ComboboxThemeDemo extends App {
+  @InlineStyleSheet(value = /* css */ """
+    .button{
+      min-width: 200px;
+    } 
+  """)
 
-  private static final String THEME = "theme";
+public class ComboboxThemeDemo extends App {
 
   @Override
   public void run() throws WebforjException {
+    
     Frame window = new Frame();
-    window.addClassName("Frame");
+
+    FlexLayout mainLayout = FlexLayout.create()
+    .vertical()
+    .justify().center()
+    .build();
+
+    FlexLayoutBuilder buttonLayout = FlexLayout.create()
+    .justify().around();
+
+    FlexLayout filledButtons = buttonLayout.build();
+    FlexLayout outlineButtons = buttonLayout.build();
 
     List<ListItem> data = new ArrayList<>();
-    data.add(new ListItem("Theme Name", "Theme Name"));
+    data.add(new ListItem("Default Selection", "Theme Name"));
     data.add(new ListItem("Random Data1", "Some Data"));
     data.add(new ListItem("Random Data2", "More Data"));
     data.add(new ListItem("Random Data3", "Test Data"));
@@ -30,21 +45,29 @@ public class ComboboxThemeDemo extends App {
     for (ButtonTheme theme : ButtonTheme.values()) {
       if (!theme.name().contains("OUTLINE")) {
         ChoiceBox themeChoiceBox = new ChoiceBox();
-        data.set(0,new ListItem("Theme Name", theme.name()));
+        data.set(0,new ListItem("Default Selection", theme.name()));
         themeChoiceBox.insert(data)
-            .selectIndex(0);
-        window.add(themeChoiceBox.setTheme(theme));
+            .selectIndex(0)
+            .addClassName("button");
+        filledButtons.add(themeChoiceBox.setTheme(theme));
       }
     }
 
     for (ButtonTheme theme : ButtonTheme.values()) {
       if (theme.name().contains("OUTLINE")) {
         ChoiceBox themeChoiceBox = new ChoiceBox();
-        data.set(0,new ListItem("Theme Name", theme.name()));
+        data.set(0,new ListItem("Default Selection", theme.name()));
         themeChoiceBox.insert(data)
-            .selectIndex(0);
-        window.add(themeChoiceBox.setTheme(theme));
+            .selectIndex(0)
+            .addClassName("button");
+        outlineButtons.add(themeChoiceBox.setTheme(theme));
       }
     }
+
+    mainLayout.setMargin("10px");
+    mainLayout.add(filledButtons, outlineButtons);
+    window.add(mainLayout);
+    
+    }
+
   }
-}
