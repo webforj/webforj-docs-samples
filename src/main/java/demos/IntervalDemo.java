312c71a1123815dcdc4a8b50ca256f00f495956b
Ben Brennan <bbrennan@basis.cloud>
2024-07-12 10:29:38 -0600

    Changes to Interval Demo Made adjustments after meeting with Matthew and Bryan.

--- src/main/java/demos/IntervalDemo.java
+++ src/main/java/demos/IntervalDemo.java
@@ -1,5 +1,6 @@
 package demos;
 
+import com.webforj.Interval;
 import com.webforj.App;
 import com.webforj.component.button.Button;
 import com.webforj.component.button.ButtonTheme;
@@ -10,63 +11,65 @@ import com.webforj.component.window.Frame;
 import com.webforj.component.field.NumberField;
 import com.webforj.dispatcher.EventListener;
 import com.webforj.exceptions.WebforjException;
-import com.webforj.Interval;
+
 import java.util.ArrayList;
 import java.util.ListIterator;
 
 public class IntervalDemo extends App {
 
-    @Override
-    public void run() throws WebforjException {
+  @Override
+  public void run() throws WebforjException {
     Frame window = new Frame();
 
-    //Creates the buttons with icons and labels//
-    Button startInterval = new Button("""
-    <html>
-        <dwc-icon name=\"player-play\"></dwc-icon>
-        Play
-    </html>
-    """);
+    // Creates the buttons with icons and labels//
+    Button startInterval = new Button(/*html */"""
+        <html>
+            <dwc-icon name=\"player-play\"></dwc-icon>
+            Play
+        </html>
+        """);
     startInterval.setEnabled(false);
 
-    Button stopInterval = new Button("""
+    Button stopInterval = new Button(/*html */"""
         <html>
             <dwc-icon name=\"player-pause\"></dwc-icon>
             Pause
         </html>
         """);
 
-    Button restartInterval = new Button("""
+    Button restartInterval = new Button(/*html */"""
         <html>
             <dwc-icon name=\"settings-automation\"></dwc-icon>
             Set Delay
         </html>
         """);
 
-        //Creates the NumberField to chose the Interval delay//
-        //Note: Fractional seconds are allowed to millisecond resolution for an Interval delay//
-        float delay = 1f;
-        double speedMin = 1;
+    // Creates the NumberField to chose the Interval delay//
+    // Note: Fractional seconds are allowed to millisecond resolution for an
+    // Interval delay//
+    float delay = 1f;
+    double speedMin = 0.01;
 
-        NumberField delaySpeed = new NumberField();
-        delaySpeed.setMin(speedMin);
-        delaySpeed.setPlaceholder("Delay Speed");
-        delaySpeed.setStyle("width","120px");
+    NumberField delaySpeed = new NumberField();
+    delaySpeed.setMin(speedMin);
+    delaySpeed.setPlaceholder("Delay Speed");
+    delaySpeed.setStyle("width", "120px");
+    delaySpeed.setStep(speedMin);
 
-
-    //Creates an array of different icons to loop through on the displayButton during the Interval event//
+    // Creates an array of different icons to loop through on the displayButton
+    // during the Interval event//
     String baby = """
         <html>
             <dwc-icon name=\"baby-carriage\"></dwc-icon>
         </html>
         """;
-    
+
     String kid = """
         <html>
             <dwc-icon name=\"skateboarding\"></dwc-icon>
         </html>
         """;
-    
+
     String adult = """
         <html>
             <dwc-icon name=\"run\"></dwc-icon>
@@ -78,7 +81,7 @@ public class IntervalDemo extends App {
             <dwc-icon name=\"old\"></dwc-icon>
         </html>
         """;
-    
+
     ArrayList<String> display = new ArrayList<>();
 
     display.add(baby);
@@ -89,60 +92,58 @@ public class IntervalDemo extends App {
     ListIterator<String> displayIterator = display.listIterator(0);
 
     Button displayButton = new Button().setTheme(ButtonTheme.OUTLINED_PRIMARY);
-    displayButton.setStyle("width","70px");
+    displayButton.setStyle("width", "70px");
 
     EventListener<Interval.ElapsedEvent> listener = (e -> {
-        if (displayIterator.hasNext()){
-            displayButton.setText(displayIterator.next());
-        }
-        //Loops back to the beginning of the array//
-        else{
-            displayIterator.previous();
-            displayIterator.previous();
-            displayIterator.previous();
-            displayButton.setText(display.get(0));
-        }
+      if (displayIterator.hasNext()) {
+        displayButton.setText(displayIterator.next());
+      }
+      // Loops back to the beginning of the array//
+      else {
+        displayIterator.previous();
+        displayIterator.previous();
+        displayIterator.previous();
+        displayButton.setText(display.get(0));
+      }
     });
 
-    //Creates the Interval that starts on run//
+    // Creates the Interval that starts on run//
     Interval interval = new Interval(delay, listener);
     interval.start();
-    
-    //Button click events//
+
     startInterval.addClickListener(e -> {
-        startInterval.setEnabled(false);
-        stopInterval.setEnabled(true);
-        interval.start();
+      startInterval.setEnabled(false);
+      stopInterval.setEnabled(true);
+      interval.start();
     });
 
     stopInterval.addClickListener(e -> {
-        startInterval.setEnabled(true);
-        stopInterval.setEnabled(false);
-        interval.stop();
+      startInterval.setEnabled(true);
+      stopInterval.setEnabled(false);
+      interval.stop();
     });
 
     restartInterval.addClickListener(e -> {
-        if (delaySpeed.getValue() != null){
-            startInterval.setEnabled(false);
-            stopInterval.setEnabled(true);
-            float pickedSpeed = delaySpeed.getValue().floatValue();
-            interval.setDelay(pickedSpeed);
-            interval.restart();
-        }
-        else{
-            ConfirmDialog error = new ConfirmDialog("Please set a delay speed.","Interval Demo");
-            error.show();
-        }
+      if (delaySpeed.getValue() != null) {
+        startInterval.setEnabled(false);
+        stopInterval.setEnabled(true);
+        float pickedSpeed = delaySpeed.getValue().floatValue();
+        interval.setDelay(pickedSpeed);
+        interval.restart();
+      } else {
+        ConfirmDialog error = new ConfirmDialog("Please set a delay speed.", "Interval Demo");
+        error.show();
+      }
     });
 
     FlexLayout mainLayout = new FlexLayout(startInterval, stopInterval, displayButton, delaySpeed, restartInterval)
-    .setJustifyContent(FlexJustifyContent.CENTER);
-    
+        .setJustifyContent(FlexJustifyContent.CENTER);
+
     mainLayout
         .setMargin("10px")
         .setPadding("10px");
 
     window.add(mainLayout);
 
-  } 
+  }
 }
