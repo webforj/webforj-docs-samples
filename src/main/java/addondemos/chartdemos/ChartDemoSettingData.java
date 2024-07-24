790137e54203a9394a7addc00e648f16e2ccb378
lauren <laurenalamo@outlook.com>
2024-07-24 13:25:49 -0400

    Refactor: Switched from List.of to Arrays.asList in ChartDemoSettingData
    - Updated ChartDemoSettingData class to use Arrays.asList instead of
    List.of to allow for null values in data set.

--- src/main/java/addondemos/chartdemos/ChartDemoSettingData.java
+++ src/main/java/addondemos/chartdemos/ChartDemoSettingData.java
@@ -6,6 +6,7 @@ import com.webforj.component.googlecharts.GoogleChart;
 import com.webforj.component.window.Frame;
 import com.webforj.exceptions.WebforjException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -30,13 +31,13 @@ public class ChartDemoSettingData extends App {
     chart.setStyle("width", "100vh");
 
     List<Object> data = new ArrayList<>();
-    data.add(List.of("Region", "Sales"));
-    data.add(List.of("North America", 500));
-    data.add(List.of("Europe", 300));
-    data.add(List.of("Asia", 200));
-    data.add(List.of("Latin America", 100));
-    data.add(List.of("Middle East", 80));
-    data.add(List.of("Africa", 60));
+    data.add(Arrays.asList("Region", "Sales"));
+    data.add(Arrays.asList("North America", 500));
+    data.add(Arrays.asList("Europe", 300));
+    data.add(Arrays.asList("Asia", 200));
+    data.add(Arrays.asList("Latin America", 100));
+    data.add(Arrays.asList("Middle East", 80));
+    data.add(Arrays.asList("Africa", 60));
     chart.setData(data);
 
     Map<String, Object> options = new HashMap<>();
@@ -44,7 +45,7 @@ public class ChartDemoSettingData extends App {
     options.put("is3D", "true");
     options.put(
         "colors",
-        List.of("#BBDEFB", "#64B5F6", "#1E88E5", "#0D47A1", "#1565C0", "#82B1FF")
+        Arrays.asList("#BBDEFB", "#64B5F6", "#1E88E5", "#0D47A1", "#1565C0", "#82B1FF")
     );
 
     chart.setOptions(options);
