61ff25a8f154fd11b2a063516426b34a3202bf82
Garrison Osteen <gosteen@basis.cloud>
2024-08-14 13:10:20 -0600

    Simplified the basic Toast Demo to showcase default behavior, style, etc.

--- src/main/java/componentdemos/toastdemos/ToastDemo.java
+++ src/main/java/componentdemos/toastdemos/ToastDemo.java
@@ -1,27 +1,19 @@
 package componentdemos.toastdemos;
 
 import com.webforj.App;
-import com.webforj.component.Theme;
 import com.webforj.component.button.Button;
-import com.webforj.component.button.ButtonTheme;
-import com.webforj.component.html.elements.Paragraph;
-import com.webforj.component.spinner.Spinner;
 import com.webforj.component.toast.Toast;
 import com.webforj.component.window.Frame;
 import com.webforj.exceptions.WebforjException;
 
 public class ToastDemo extends App {
-
-  Toast t = new Toast("", -1, Theme.GRAY);
-
+  int counter = 1;
   @Override
   public void run() throws WebforjException {
+    
     Frame frame = new Frame();
-    frame.add(t);
-    t.add(
-        new Spinner(),
-        new Paragraph("System update failed. Restoring to the previous state."),
-        new Button("Stop", ButtonTheme.DANGER, e -> t.close()));
-    t.open();
+    frame.add(new Button("Make Toast", e -> {
+      Toast.show("Toast Notification " + counter++);
+    }));
   }
 }
