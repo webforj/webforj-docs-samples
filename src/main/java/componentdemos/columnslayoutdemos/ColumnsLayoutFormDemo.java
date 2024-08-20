296a19a45fbfa4e3a24105d4a65808bbabec0c1b
Ben Brennan <bbrennan@basis.cloud>
2024-08-20 10:10:49 -0600

    ColumnsLayout Form Demo Changes * Replaced setLabel methods with the constructors that have label as an argument * Formatted spacing

--- src/main/java/componentdemos/columnslayoutdemos/ColumnsLayoutFormDemo.java
+++ src/main/java/componentdemos/columnslayoutdemos/ColumnsLayoutFormDemo.java
@@ -32,13 +32,13 @@ public class ColumnsLayoutFormDemo extends App {
 
     Frame frame = new Frame();
 
-    email = new TextField().setLabel("Email");
-    password = new PasswordField().setLabel("Password");
-    firstName = new TextField().setLabel("First Name");
-    lastName = new TextField().setLabel("Last Name");
-    address = new TextField().setLabel("Address");
-    city = new TextField().setLabel("City");
-    zip = new MaskedNumberField().setLabel("Zip");
+    email = new TextField("Email");
+    password = new PasswordField("Password");
+    firstName = new TextField("First Name");
+    lastName = new TextField("Last Name");
+    address = new TextField("Address");
+    city = new TextField("City");
+    zip = new MaskedNumberField("Zip");
 
     populateStates();
 
@@ -46,85 +46,83 @@ public class ColumnsLayoutFormDemo extends App {
     cancel = new Button("Cancel", ButtonTheme.DEFAULT);
 
     ColumnsLayout layoutOne = new ColumnsLayout(email, password, firstName, lastName, address)
-      .setSpan(address, 2)
-      .setSpacing(10);
+        .setSpan(address, 2)
+        .setSpacing(10);
 
     List<Breakpoint> listButtonsBreaks = List.of(
-      new Breakpoint("small",100,1),
-      new Breakpoint("medium",150,2)
-    );
+        new Breakpoint("small", 100, 1),
+        new Breakpoint("medium", 150, 2));
 
     ColumnsLayout layoutButtons = new ColumnsLayout(listButtonsBreaks, submit, cancel);
 
     List<Breakpoint> listBreaks = List.of(
-      new Breakpoint("small",200,1),
-      new Breakpoint("medium",400,2),
-      new Breakpoint("large",800,4),
-      new Breakpoint("xlarge",1250,4)
-    );
+        new Breakpoint("small", 200, 1),
+        new Breakpoint("medium", 400, 2),
+        new Breakpoint("large", 800, 4),
+        new Breakpoint("xlarge", 1250, 4));
 
     ColumnsLayout layoutTwo = new ColumnsLayout(listBreaks, city, states, zip, layoutButtons)
-      .setSpan(city, "large", 2)
-      .setSpan(layoutButtons, "large", 2)
-      .setColumn(layoutButtons, "large", 3)
-      .setVerticalAlignment(layoutButtons, ColumnsLayout.Alignment.END)
-      .setSpacing(10);
-  
+        .setSpan(city, "large", 2)
+        .setSpan(layoutButtons, "large", 2)
+        .setColumn(layoutButtons, "large", 3)
+        .setVerticalAlignment(layoutButtons, ColumnsLayout.Alignment.END)
+        .setSpacing(10);
+
     frame.setStyle("margin", "10px 20%");
     frame.add(layoutOne, layoutTwo);
-    }
+  }
 
-    private void populateStates() {
-      states = new ChoiceBox().setLabel("State").setMaxRowCount(7);
-      states.add("AL", "Alabama");
-      states.add("AK", "Alaska");
-      states.add("AZ", "Arizona");
-      states.add("AR", "Arkansas");
-      states.add("CA", "California");
-      states.add("CO", "Colorado");
-      states.add("CT", "Connecticut");
-      states.add("DE", "Delaware");
-      states.add("FL", "Florida");
-      states.add("GA", "Georgia");
-      states.add("HI", "Hawaii");
-      states.add("ID", "Idaho");
-      states.add("IL", "Illinois");
-      states.add("IN", "Indiana");
-      states.add("IA", "Iowa");
-      states.add("KS", "Kansas");
-      states.add("KY", "Kentucky");
-      states.add("LA", "Louisiana");
-      states.add("ME", "Maine");
-      states.add("MD", "Maryland");
-      states.add("MA", "Massachusetts");
-      states.add("MI", "Michigan");
-      states.add("MN", "Minnesota");
-      states.add("MS", "Mississippi");
-      states.add("MO", "Missouri");
-      states.add("MT", "Montana");
-      states.add("NE", "Nebraska");
-      states.add("NV", "Nevada");
-      states.add("NH", "New Hampshire");
-      states.add("NJ", "New Jersey");
-      states.add("NM", "New Mexico");
-      states.add("NY", "New York");
-      states.add("NC", "North Carolina");
-      states.add("ND", "North Dakota");
-      states.add("OH", "Ohio");
-      states.add("OK", "Oklahoma");
-      states.add("OR", "Oregon");
-      states.add("PA", "Pennsylvania");
-      states.add("RI", "Rhode Island");
-      states.add("SC", "South Carolina");
-      states.add("SD", "South Dakota");
-      states.add("TN", "Tennessee");
-      states.add("TX", "Texas");
-      states.add("UT", "Utah");
-      states.add("VT", "Vermont");
-      states.add("VA", "Virginia");
-      states.add("WA", "Washington");
-      states.add("WV", "West Virginia");
-      states.add("WI", "Wisconsin");
-      states.add("WY", "Wyoming");
+  private void populateStates() {
+    states = new ChoiceBox("State").setMaxRowCount(7);
+    states.add("AL", "Alabama");
+    states.add("AK", "Alaska");
+    states.add("AZ", "Arizona");
+    states.add("AR", "Arkansas");
+    states.add("CA", "California");
+    states.add("CO", "Colorado");
+    states.add("CT", "Connecticut");
+    states.add("DE", "Delaware");
+    states.add("FL", "Florida");
+    states.add("GA", "Georgia");
+    states.add("HI", "Hawaii");
+    states.add("ID", "Idaho");
+    states.add("IL", "Illinois");
+    states.add("IN", "Indiana");
+    states.add("IA", "Iowa");
+    states.add("KS", "Kansas");
+    states.add("KY", "Kentucky");
+    states.add("LA", "Louisiana");
+    states.add("ME", "Maine");
+    states.add("MD", "Maryland");
+    states.add("MA", "Massachusetts");
+    states.add("MI", "Michigan");
+    states.add("MN", "Minnesota");
+    states.add("MS", "Mississippi");
+    states.add("MO", "Missouri");
+    states.add("MT", "Montana");
+    states.add("NE", "Nebraska");
+    states.add("NV", "Nevada");
+    states.add("NH", "New Hampshire");
+    states.add("NJ", "New Jersey");
+    states.add("NM", "New Mexico");
+    states.add("NY", "New York");
+    states.add("NC", "North Carolina");
+    states.add("ND", "North Dakota");
+    states.add("OH", "Ohio");
+    states.add("OK", "Oklahoma");
+    states.add("OR", "Oregon");
+    states.add("PA", "Pennsylvania");
+    states.add("RI", "Rhode Island");
+    states.add("SC", "South Carolina");
+    states.add("SD", "South Dakota");
+    states.add("TN", "Tennessee");
+    states.add("TX", "Texas");
+    states.add("UT", "Utah");
+    states.add("VT", "Vermont");
+    states.add("VA", "Virginia");
+    states.add("WA", "Washington");
+    states.add("WV", "West Virginia");
+    states.add("WI", "Wisconsin");
+    states.add("WY", "Wyoming");
   }
 }
