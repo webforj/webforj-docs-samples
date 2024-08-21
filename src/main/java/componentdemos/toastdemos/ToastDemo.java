a34bc6e12404af5800d79d72a198305c2be04c3f
Garrison Osteen <gosteen@basis.cloud>
2024-08-21 12:48:52 -0600

    Added a numberfield to allow duration to be changed for the toast.

--- src/main/java/componentdemos/toastdemos/ToastDemo.java
+++ src/main/java/componentdemos/toastdemos/ToastDemo.java
@@ -3,6 +3,7 @@ package componentdemos.toastdemos;
 import com.webforj.App;
 import com.webforj.component.Expanse;
 import com.webforj.component.button.Button;
+import com.webforj.component.field.NumberField;
 import com.webforj.component.toast.Toast;
 import com.webforj.component.window.Frame;
 import com.webforj.exceptions.WebforjException;
@@ -16,12 +17,15 @@ public class ToastDemo extends App {
   public void run() throws WebforjException {
     frame = new Frame();
     frame.setStyle("padding", "1em");
-
+    NumberField durationField = new NumberField("Duration", 3000.0);
+    durationField.setStep(100.0);
     toastButton = new Button("Make Toast", e -> {
-      Toast.show("Toast Notification " + counter++);
+      Toast.show("Toast Notification " + counter++, durationField.getValue().intValue());
     });
     
     toastButton.setExpanse(Expanse.LARGE);
+    durationField.setExpanse(Expanse.LARGE);
+    frame.add(durationField);
     frame.add(toastButton);
   }
 }
