dd2edad0f3c87290a3982ce448637bb68add54f0
Ben Brennan <bbrennan@basis.cloud>
2024-09-06 10:09:10 -0600

    ColumnsLayoutChangingColumns Changes Replaced menu buttons with three radio buttons.

--- src/main/java/componentdemos/columnslayoutdemos/ColumnsLayoutChangingColumns.java
+++ src/main/java/componentdemos/columnslayoutdemos/ColumnsLayoutChangingColumns.java
@@ -5,8 +5,8 @@ import java.util.List;
 import com.webforj.component.layout.columnslayout.ColumnsLayout;
 import com.webforj.component.layout.columnslayout.ColumnsLayout.Breakpoint;
 import com.webforj.App;
-import com.webforj.component.button.Button;
-import com.webforj.component.button.ButtonTheme;
+import com.webforj.component.optioninput.RadioButton;
+import com.webforj.component.optioninput.RadioButtonGroup;
 import com.webforj.component.window.Frame;
 import com.webforj.exceptions.WebforjException;
 
@@ -15,20 +15,19 @@ public class ColumnsLayoutChangingColumns extends App{
   public void run() throws WebforjException {
     Frame frame = new Frame();    
 
-    Button home = new Button("Home", ButtonTheme.PRIMARY);
-    home.setMaxWidth("150px");
-    Button inbox = new Button("Inbox");
-    inbox.setMaxWidth("150px");
-    Button settings = new Button("Settings");
-    settings.setMaxWidth("150px");
+    RadioButton agree = new RadioButton("Agree");
+    RadioButton neutral = new RadioButton("Neutral");
+    RadioButton disagree = new RadioButton("Disagree");
+
+    RadioButtonGroup group = new RadioButtonGroup(agree, neutral, disagree);
     
     List<Breakpoint> breaks = List.of(
       new Breakpoint("one", 0, 1),
       new Breakpoint("three", 500, 3));
 
-    ColumnsLayout layout = new ColumnsLayout(breaks, home, inbox, settings);
+    ColumnsLayout layout = new ColumnsLayout(breaks, group, agree, neutral, disagree);
     
     frame.add(layout);
-    frame.setStyle("margin", "10px 20%");
+    frame.setStyle("margin", "20px");
   }
 }
