d1cd1c2d02c8c76d1911d3b2d6db386105d59433
lauren <laurenalamo@outlook.com>
2024-09-10 08:18:55 -0400

    feat: Add Toast demo for placements

---
+++ src/main/java/componentdemos/toastdemos/ToastPlacementDemo.java
@@ -0,0 +1,82 @@
+package componentdemos.toastdemos;
+
+import com.webforj.App;
+import com.webforj.annotation.InlineStyleSheet;
+import com.webforj.component.button.Button;
+import com.webforj.component.button.ButtonTheme;
+import com.webforj.component.list.ChoiceBox;
+import com.webforj.component.list.ListItem;
+import com.webforj.component.toast.Toast;
+import com.webforj.component.toast.Toast.Placement;
+import com.webforj.component.window.Frame;
+import com.webforj.exceptions.WebforjException;
+import java.util.ArrayList;
+import java.util.List;
+
+@InlineStyleSheet(/*css */"""
+    .window {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      margin: 20px;
+      gap: 50px;
+    }
+    """)
+public class ToastPlacementDemo extends App {
+
+  @Override
+  public void run() throws WebforjException {
+    Frame frame = new Frame();
+    frame.addClassName("window");
+
+    final ChoiceBox placementChoiceBox = new ChoiceBox("Select Toast Placement");
+
+    List<ListItem> placementOptions = new ArrayList<>();
+    placementOptions.add(new ListItem("TOP"));
+    placementOptions.add(new ListItem("TOP_LEFT"));
+    placementOptions.add(new ListItem("TOP_RIGHT"));
+    placementOptions.add(new ListItem("CENTER"));
+    placementOptions.add(new ListItem("BOTTOM"));
+    placementOptions.add(new ListItem("BOTTOM_LEFT"));
+    placementOptions.add(new ListItem("BOTTOM_RIGHT"));
+
+    placementChoiceBox.insert(placementOptions);
+    placementChoiceBox.selectIndex(0); 
+
+    Button showToastButton = new Button("Show Toast", e -> {
+      String selectedValue = placementChoiceBox.getText();
+
+      Placement selectedPlacement;
+      switch (selectedValue) {
+        case "TOP_LEFT":
+          selectedPlacement = Placement.TOP_LEFT;
+          break;
+        case "TOP_RIGHT":
+          selectedPlacement = Placement.TOP_RIGHT;
+          break;
+        case "CENTER":
+          selectedPlacement = Placement.CENTER;
+          break;
+        case "BOTTOM":
+          selectedPlacement = Placement.BOTTOM;
+          break;
+        case "BOTTOM_LEFT":
+          selectedPlacement = Placement.BOTTOM_LEFT;
+          break;
+        case "BOTTOM_RIGHT":
+          selectedPlacement = Placement.BOTTOM_RIGHT;
+          break;
+        case "TOP":
+        default:
+          selectedPlacement = Placement.TOP;
+          break;
+      }
+
+      Toast.show("This is a toast notification", selectedPlacement);
+    });
+    showToastButton.setTheme(ButtonTheme.PRIMARY);
+    
+    frame.add(placementChoiceBox);
+    frame.add(showToastButton);
+  }
+}
