f748acea5434bde956abc7550d9e7a17767454e5
MatthewHawkins <matthew.hawkins1115@gmail.com>
2024-10-17 17:54:26 +0200

    chore: removing debug for routing and console log for main app from main branch

--- src/main/java/com/webforj/samples/Application.java
+++ src/main/java/com/webforj/samples/Application.java
@@ -5,12 +5,11 @@ import com.webforj.annotation.AppTitle;
 import com.webforj.annotation.Routify;
 import com.webforj.exceptions.WebforjException;
 
-@Routify(packages = "com.webforj.samples.views", debug = true)
+@Routify(packages = "com.webforj.samples.views")
 @AppTitle("webforJ Samples")
 public class Application extends App{
   
   @Override
   public void run() throws WebforjException {
-    console().log("Test");
   }
 }
