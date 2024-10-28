216696fb18c81bb2a21aa7269f0cf9b178423c9e
Lauren Alamo <laurenalamo@outlook.com>
2024-10-28 14:48:18 -0400

    made demo responsive and added labels/values

--- src/main/java/com/webforj/samples/views/fields/textfield/TextFieldView.java
+++ src/main/java/com/webforj/samples/views/fields/textfield/TextFieldView.java
@@ -8,6 +8,7 @@ import com.webforj.component.html.elements.Div;
 import com.webforj.component.html.elements.Paragraph;
 import com.webforj.component.layout.flexlayout.FlexDirection;
 import com.webforj.component.layout.flexlayout.FlexLayout;
+import com.webforj.component.layout.flexlayout.FlexWrap;
 import com.webforj.component.window.Frame;
 import com.webforj.router.annotation.FrameTitle;
 import com.webforj.router.annotation.Route;
@@ -18,22 +19,27 @@ public class TextFieldView extends Composite<FlexLayout> {
 
   public TextFieldView() {
     getBoundComponent().setMargin("var(--dwc-space-l)").setSpacing("var(--dwc-space-l)")
-    .setDirection(FlexDirection.ROW);
+    .setDirection(FlexDirection.ROW).setWrap(FlexWrap.WRAP);
         
     TextField nameField = new TextField();
-    nameField.setPlaceholder("Name").setType(TextField.Type.TEXT);
+    nameField.setPlaceholder("Name").setType(TextField.Type.TEXT).setLabel("Enter Name")
+    .setValue("John Doe");
         
     TextField emailField = new TextField();
-    emailField.setPlaceholder("Email").setType(TextField.Type.EMAIL);
+    emailField.setPlaceholder("Email").setType(TextField.Type.EMAIL).setLabel("Enter Email")
+    .setValue("example@email.com");
         
     TextField telField = new TextField();
-    telField.setPlaceholder("Phone Number").setType(TextField.Type.TEL);
+    telField.setPlaceholder("Phone Number").setType(TextField.Type.TEL).setLabel("Enter Phone Number")
+    .setValue("(123) 456-7890");
         
     TextField urlField = new TextField();
-    urlField.setPlaceholder("URL").setType(TextField.Type.URL);
+    urlField.setPlaceholder("URL").setType(TextField.Type.URL).setLabel("Enter URL")
+    .setValue("https://www.example.com");
         
     TextField searchField = new TextField();
-    searchField.setPlaceholder("Search").setType(TextField.Type.SEARCH);
+    searchField.setPlaceholder("Search").setType(TextField.Type.SEARCH).setLabel("Enter Your Search")
+    .setValue("Search...");
     
     getBoundComponent().add(nameField, emailField, telField, urlField, searchField);
 
