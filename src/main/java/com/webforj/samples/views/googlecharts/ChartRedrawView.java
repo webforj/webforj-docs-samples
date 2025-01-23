928ffca85fd84aafb59eab0350aeccc2d87ca12b
Lauren Alamo <laurenalamo@outlook.com>
2025-01-23 13:57:43 -0500

    added inputs to flex container for smaller screens

--- src/main/java/com/webforj/samples/views/googlecharts/ChartRedrawView.java
+++ src/main/java/com/webforj/samples/views/googlecharts/ChartRedrawView.java
@@ -7,7 +7,9 @@ import com.webforj.component.button.ButtonTheme;
 import com.webforj.component.field.NumberField;
 import com.webforj.component.googlecharts.GoogleChart;
 import com.webforj.component.html.elements.Div;
+import com.webforj.component.layout.flexlayout.FlexJustifyContent;
 import com.webforj.component.layout.flexlayout.FlexLayout;
+import com.webforj.component.layout.flexlayout.FlexWrap;
 import com.webforj.router.annotation.FrameTitle;
 import com.webforj.router.annotation.Route;
 import java.util.ArrayList;
@@ -26,7 +28,6 @@ public class ChartRedrawView extends Composite<Div> {
 
   private final GoogleChart chart = new GoogleChart(GoogleChart.Type.COLUMN);
   private final Button redrawButton = new Button("Redraw Chart");
-  private final FlexLayout inputGroup = new FlexLayout();
 
   public ChartRedrawView() {
     getBoundComponent().addClassName("window");
@@ -49,13 +50,20 @@ public class ChartRedrawView extends Composite<Div> {
     List<String> columns = List.of("Category", "Number of Followers in Thousands");
     data.add(columns);
 
+
+    FlexLayout inputContainer = new FlexLayout();
+    inputContainer.addClassName("input-container");
+    inputContainer.setJustifyContent(FlexJustifyContent.CENTER);
+    inputContainer.setWrap(FlexWrap.WRAP); 
+    inputContainer.setSpacing("10px"); 
+    
     String[] categories = {"Instagram", "Twitter", "Facebook", "LinkedIn"};
     Map<String, NumberField> valueFields = new HashMap<>();
-
+    
     for (String category : categories) {
       List<Object> row = new ArrayList<>();
       row.add(category);
-      row.add(100); 
+      row.add(100);
       data.add(row);
 
       NumberField valueField = new NumberField("Value for " + category);
@@ -63,7 +71,7 @@ public class ChartRedrawView extends Composite<Div> {
       valueField.setStep(1.0);
       valueField.addClassName("number-field");
       valueField.setText("100");
-      inputGroup.add(valueField);
+      inputContainer.add(valueField);
       valueFields.put(category, valueField);
     }
 
@@ -76,7 +84,7 @@ public class ChartRedrawView extends Composite<Div> {
     redrawButton.addClassName("redraw-button");
     redrawButton.addClickListener(e -> {
       List<Object> newData = new ArrayList<>();
-      newData.add(columns); 
+      newData.add(columns);
       boolean allValuesValid = true;
 
       for (String category : categories) {
@@ -105,10 +113,6 @@ public class ChartRedrawView extends Composite<Div> {
       }
     });
 
-    getBoundComponent().addClassName("window");
-    getBoundComponent().add(chartContainer);
-    getBoundComponent().add(inputGroup);
-    getBoundComponent().add(buttonContainer); 
+    getBoundComponent().add(chartContainer, inputContainer, buttonContainer);
   }
-
 }
